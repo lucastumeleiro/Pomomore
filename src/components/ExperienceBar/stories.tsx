1538499219d9c83a5874c384bcd9cf196a57dfcc
Lucas Tumeleiro <49693585+lucastumeleiro@users.noreply.github.com>
2022-04-07 00:05:14 -0300

    retirado tentativa de aplicação de storybook

--- src/components/ExperienceBar/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react/types-6-0'
-import ExperienceBar from '.'
-
-export default {
-  title: 'ExperienceBar',
-  component: ExperienceBar
-} as Meta
-
-export const Default: Story = (args) => <ExperienceBar {...args} />
--- src/components/Profile/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react/types-6-0'
-import Profile from '.'
-
-export default {
-  title: 'Profile',
-  component: Profile
-} as Meta
-
-export const Default: Story = (props) => <Profile {...props} />
